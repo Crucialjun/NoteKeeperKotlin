670782383e459e98794a7effda4271b8d6d044a4
Crucialjun <nich.otieno@gmail.com>
2020-12-14 10:05:38 +0300

    recyclerview inflate view

--- app/src/main/java/com/example/notekeeperkotlin/NoteRecyclerAdapter.kt
+++ app/src/main/java/com/example/notekeeperkotlin/NoteRecyclerAdapter.kt
@@ -4,16 +4,22 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class NoteRecyclerAdapter(context: Context) : RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>() {
 
-    val layoutInflater = LayoutInflater.from(context)
+    private val layoutInflater: LayoutInflater = LayoutInflater.from(context)
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textCourse = itemView.findViewById<TextView>(R.id.text_course)
+        val textTitle = itemView.findViewById<TextView>(R.id.text_title)
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val itemView = layoutInflater.inflate(R.layout.item_note_list, parent, false)
+        return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
